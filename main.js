e6fbb11c5a69882bb64918f5512b9426166e57e1
Valeria Ciccone <valeria@MacBook-Air-di-Valeria.local>
2019-04-07 15:26:39 +0100

    Main page hover
    Trying to fix the garbage pile showing on the main page when hovering. Last version of this is been set as comment

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-$( ".hoverover" ).hover(function() {
-  $( ".garbage" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-});
+
+//TEST
+// $( ".hoverover" ).hover(function() {
+//   $( ".garbage" ).toggle( "slow", function() {
+//     // Animation complete.
+//   });
+// });
+const title = document.getElementsByClassName ("title main")
+const garbage = document.getElementById("garbage")
+
+title.addEventListener ("mouseover", showGarbage)
+function showGarbage() {
+  title.style.display = "none"
+  garbage.style.display = "block"
+}
