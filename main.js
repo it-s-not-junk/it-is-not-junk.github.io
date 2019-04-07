a9327142743cdf75a7fc7cc5da7d9e093acda6da
Valeria Ciccone <valeria@MacBook-Air-di-Valeria.local>
2019-04-07 16:00:39 +0100

    Changed hover on title main page
    Now when you hover over the tile you should get a pile of garbage, when you don't it should go away, but there is a glitch

--- main.js
+++ main.js
@@ -5,11 +5,17 @@
 //     // Animation complete.
 //   });
 // });
-const title = document.getElementsByClassName ("title main")
+const title = document.getElementById ("title")
 const garbage = document.getElementById("garbage")
 
 title.addEventListener ("mouseover", showGarbage)
 function showGarbage() {
   title.style.display = "none"
   garbage.style.display = "block"
+}
+
+title.addEventListener ("mouseout", hideGarbage)
+function hideGarbage() {
+  title.style.display = "block"
+  garbage.style.display = "none"
 }
