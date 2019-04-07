201d6a0503173414e6f1c3f7a61729034b59d13e
Valeria Ciccone <valeria@MacBook-Air-di-Valeria.local>
2019-04-07 19:46:23 +0100

    questions now showing on quiz.js
    fixed quiz.js to now show the first question. Moved some lines to try and make it more clear

--- quiz.js
+++ quiz.js
@@ -1,13 +1,49 @@
 const start = document.getElementById ("start")
-const quiz = document.getElementById('quiz')
-const question = document.getElementById('question')
-const qImg = document.getElementById('qImg')
-const choices = document.getElementById('choiches')
-const choiceA = document.getElementById('A')
-const choiceB = document.getElementById('B')
-const choiceC = document.getElementById('C')
+const quiz = document.getElementById("quiz")
+const question = document.getElementById("question")
+const qImg = document.getElementById("qImg")
+const choices = document.getElementById("choiches")
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const choiceC = document.getElementById("C")
+
+// array with questions that will be displayed
+let questions = [
+  {
+    question: "How much plastic is trown in the ocean each year?",
+    imgSrc : "plasicwave.png",
+    choiceA : "8 million metric tons",
+    choiceB : "2 million metric tons",
+    choicheC : "none, we went plastic free",
+    correct : "A"
+  },{
+    question: "How much trash could be given new life?",
+    imgSrc : "recycling.png",
+    choiceA : "about 60%",
+    choiceB : "about 80%",
+    choicheC : "about 15%",
+    correct : "B"
+  },{
+    question : "Which contries use more plastic bottles?",
+    imgSrc : "plastic bottles.png",
+    choiceA : "Italy, Germany and France",
+    choiceB : "Indonesia, Brazil and Thailand",
+    choicheC : "USA, China and Mexico",
+    correct : "C"
+  }
+]
 
 let runningQuestion = 0;
+
+function renderQuestion(){
+  let q = questions[runningQuestion];
+
+  question.innerHTML = "<p>"+ q.question +"</p>";
+  qImg.innerHTML = "<img src:"+ q.imgSrc +"/>"
+  choicheA.innerHTML = q.choicheA
+  choicheB.innerHTML = q.choicheB
+  choicheC.innerHTML = q.choicheC
+}
 
 start.addEventListener ("click", startQuiz)
 function startQuiz() {
@@ -15,33 +51,3 @@ function startQuiz() {
   renderQuestion()
   quiz.style.display = "block";
 }
-
-function renderQuestion(){
-  let q = questions[runningQuestion];
-
-  question.innerHTML = "<p>"+ q.question +"</p>";
-}
-
-let questions = [
-  {question: "How much plastic is trown in the ocean each year?"
-  qImg: "plasicwave.png"
-  choiceA: "8 million metric tons"
-  choiceB: "2 million metric tons"
-  choicheC: "none, we went plastic free"
-  correct: "A"
-  },{
-  question: "How much trash could be given new life?"
-  qImg: "recycling.png"
-  choiceA: "about 60%"
-  choiceB: "about 80%"
-  choicheC: "about 15%"
-  correct: "B"
-  },{
-  question: "Which contries use more plastic bottles?"
-  qImg: "plastic bottles.png"
-  choiceA: "Italy, Germany and France"
-  choiceB: "Indonesia, Brazil and Thailand"
-  choicheC: "USA, China and Mexico"
-  correct: "C"
-  }
-]
