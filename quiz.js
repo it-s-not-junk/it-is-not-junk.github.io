96813608624031621f134a6aba82114e132d8abd
Valeria Ciccone <valeria@MacBook-Air-di-Valeria.local>
2019-04-07 11:54:06 +0100

    Fixed bug quiz.js
    Now the quiz starts when you press the right button. Questions don't show yet

--- quiz.js
+++ quiz.js
@@ -3,7 +3,6 @@ const quiz = document.getElementById('quiz')
 const question = document.getElementById('question')
 const  qImg = document.getElementById('qImg')
 const choices = document.getElementById('choiches')
-const choices = document.getElementByclass('choiche')
 const choiceA = document.getElementById('A')
 const choiceB = document.getElementById('B')
 const choiceC = document.getElementById('C')
@@ -11,7 +10,7 @@ const scorecontainer = document.getElementById('scorecontainer')
 
 // This function is not working
 start.addEventListener ("click", startQuiz)
-function startQuiz(){
+function startQuiz() {
   start.style.display = "none";
   quiz.style.display = "block";
 }
